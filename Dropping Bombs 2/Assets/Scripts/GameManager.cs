651876084aa763d3acdff8510693082464459e2f
fundamental-group <emathstutorials@gmail.com>
2022-12-10 16:17:36 -0800

    Dropping Bombs 3 (MOST)

---
+++ Dropping Bombs 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+
+    void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner.active = false;
+        title.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            spawner.active = true;
+            title.SetActive(false);
+        }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y)-12)
+            {
+                Destroy(bombObject);
+            }
+        }
+
+    }
+}
